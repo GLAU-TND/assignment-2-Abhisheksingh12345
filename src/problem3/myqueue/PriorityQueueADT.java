3503b9879f936051a749b5a0b53ab1df7b98bbad
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 15:08:29 +0530

    these are function which will give an behaviour to the PriorityQueue. E --> its means generic.

--- src/problem3/myqueue/PriorityQueueADT.java
+++ src/problem3/myqueue/PriorityQueueADT.java
@@ -1,5 +1,11 @@
 package problem3.myqueue;
 
 public interface PriorityQueueADT {
+    boolean add(E data);
 
+    boolean traverse();
+
+    boolean delete(E data);
+
+    boolean search(E data);
 }
