80df3452044cf1bacc0c5ccaa2211153445823fa
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 15:09:46 +0530

    comments

--- src/problem3/myqueue/PriorityQueueADT.java
+++ src/problem3/myqueue/PriorityQueueADT.java
@@ -1,11 +1,15 @@
 package problem3.myqueue;
 
-public interface PriorityQueueADT {
+public interface PriorityQueueADT<E> {
+    // to add data
     boolean add(E data);
 
+    // to print data
     boolean traverse();
 
+    // to delete from queue
     boolean delete(E data);
 
+    // to search from queue
     boolean search(E data);
 }
