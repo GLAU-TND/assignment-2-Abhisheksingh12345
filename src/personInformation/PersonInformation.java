7e6eae507bfc69b788a25e28424a18bd59ded437
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 18:35:01 +0530

    this class bring out the information of Person.

---
+++ src/personInformation/PersonInformation.java
@@ -0,0 +1,34 @@
+package personInformation;
+
+public class PersonInformation {
+    private String firstName;
+    private String lastName;
+    private int rollNo;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+}
+
+
