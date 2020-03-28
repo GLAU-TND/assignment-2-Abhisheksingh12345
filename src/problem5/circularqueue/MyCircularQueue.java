e86aacf472fcff0b1edaa3e683c7cb1f7b01006f
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 21:17:09 +0530

    this is the override function of the MyCircularQueueADT function

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -6,5 +6,19 @@
  */
 package problem5.circularqueue;
 //to implement circular queue
-public class MyCircularQueue {
+public class MyCircularQueue<E> implements MyCircularQueueADT<E> {
+    @Override
+    public boolean enqueue(E data) {
+        return false;
+    }
+
+    @Override
+    public boolean dequeue() {
+        return false;
+    }
+
+    @Override
+    public boolean search(E data) {
+        return false;
+    }
 }
