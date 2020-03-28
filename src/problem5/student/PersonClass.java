e9c7126096a89ab28a42401a4d4faa4dc2f7a2be
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 21:11:07 +0530

    getter and setter and constructor containing all the fields for initializing .

--- src/problem5/student/PersonClass.java
+++ src/problem5/student/PersonClass.java
@@ -5,9 +5,43 @@
  *  Time: 7:06 PM
  */
 package problem5.student;
+
 // to store student information and properties
 public class PersonClass {
+    private String name;
+    private int age;
+    private long rollNo;
 
-    public PersonClass(int i, String loki) {
+    public PersonClass(String name, int age, long rollNo) {
+        this.name = name;
+        this.age = age;
+        this.rollNo = rollNo;
+    }
+
+    public PersonClass(int age, String Name) {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(long rollNo) {
+        this.rollNo = rollNo;
     }
 }
