80335934fb22d10ba2552f83ad4419fcc8c59ec5
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 11:24:53 +0530

    these are the getter and setter for the field.

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -6,5 +6,32 @@
  */
 package problem1.node;
 
-public class TreeNode {
+public class TreeNode<E> {
+    private TreeNode rightChild;
+    private TreeNode leftChild;
+    private E data;
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
 }
