8f5fb036a66fea178e60179d4e7696a224d88d6e
Mokey-D-luffy88 <atching123@gmail.com>
2020-03-28 21:22:25 +0530

    these are some specific fields front --> this is the reference for the 1'st Node. rear --> this is the reference for the last Node. size --> user have to specifed size

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -5,11 +5,22 @@
  *  Time: 7:06 PM
  */
 package problem5.circularqueue;
+import problem5.node.*;
+
 //to implement circular queue
 public class MyCircularQueue<E> implements MyCircularQueueADT<E> {
+    private Node<E> front = null;
+    private Node<E> rear = null;
+    private int size = 0;
+
+    public void giveQueueSize(int size) {
+        this.size = size;
+    }
+
     @Override
     public boolean enqueue(E data) {
-        return false;
+        addFromFront(data, size);
+
     }
 
     @Override
